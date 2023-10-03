7d1e8b618306841129efcceadd61fa5383a59782
Stephen York <stephen.york@dewr.gov.au>
2023-10-03 15:23:03 +1100

    Added page not found view

--- RealWorldVue/src/router/index.js
+++ RealWorldVue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import EventListView from '../views/EventListView.vue';
 import EventDetailsView from '../views/EventDetailsView.vue';
 import AboutView from '../views/AboutView.vue';
+import PageNotFoundView from '../views/PageNotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: PageNotFoundView,
+      meta: {
+        requiresAuth: false
+      }
     }
   ]
 })
---
+++ RealWorldVue/src/views/PageNotFoundView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>Requested Page Not Found</div>
+</template>
+
+<style scoped>
+
+</style>
